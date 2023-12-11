eac6df5ffe69a82a452e6bc93482b4e869f14c0c
brianbancroft <hello@brianbancroft.ca>
2023-12-11 12:02:24 -0800

    Create base layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,32 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
+import {
+  QuestionMarkCircleIcon,
+  FolderOpenIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/24/outline";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main>
-      <h1>hello world</h1>
-      <div className="text-red-500">goo bar</div>
-    </main>
+    <div className="grid grid-rows-[1fr_100px] overflow-hidden min-h-screen">
+      <main className="overflow-y-scroll">hello world</main>
+      <footer className="text-red-500 bg-slate-300 flex justify-evenly py-4">
+        <a className="flex flex-col">
+          <FolderOpenIcon />
+          <p>archive</p>
+        </a>
+        <a className="flex flex-col">
+          <PlusCircleIcon />
+          <p>Assess</p>
+        </a>
+        <a className="flex flex-col">
+          <QuestionMarkCircleIcon /> <p>about</p>
+        </a>
+      </footer>
+    </div>
   );
 }
 
