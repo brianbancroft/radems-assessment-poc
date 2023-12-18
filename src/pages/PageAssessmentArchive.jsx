0fe9ba2fb17dd118b0a9e57c07bc3afcb409f706
brianbancroft <hello@brianbancroft.ca>
2023-12-17 21:46:31 -0800

    Create archive index page

--- src/pages/PageAssessmentArchive.jsx
+++ src/pages/PageAssessmentArchive.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { useMemo } from "react";
+import { Link } from "react-router-dom";
 
 function PageAssessmentArchive() {
-  return <div>PageAssessmentArchive</div>;
+  const assessments = useMemo(() => {
+    const savedAssessments = JSON.parse(
+      localStorage.getItem("assessment") ?? "{}"
+    );
+
+    const presentableAssessments = Object.values(savedAssessments).sort(
+      (a, b) => {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      }
+    );
+
+    return presentableAssessments;
+  }, []);
+
+  return (
+    <div className="bg-gray-200 ">
+      <article className="max-w-prose bg-white min-h-screen mx-auto">
+        <h3>Assessments</h3>
+        <ul>
+          {assessments.map((assessment) => (
+            <li
+              key={assessment.id}
+              className="p-4 bg-gray-100 mx-2 rounded flex justify-between"
+            >
+              <div>
+                <h4>{assessment.name}</h4>
+                <p>{assessment.description}</p>
+              </div>
+              <div>
+                <Link to={`/archive/${assessment.id}`} className="bg-white p-4">
+                  <button>View</button>
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </div>
+  );
 }
 
 export default PageAssessmentArchive;
