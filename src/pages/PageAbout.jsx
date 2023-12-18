bb958b9a05c1f9ca91bedceb6c24db99d4615bca
brianbancroft <hello@brianbancroft.ca>
2023-12-17 22:21:00 -0800

    Add about page

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -1,12 +1,28 @@
+import githubIcon from "../lib/github-icon.svg";
+
 function PageAbout() {
   return (
-    <article>
-      <p>
-        This is a proof of concept, and something I thought I could snap up in a
-        few afternoons. RadEMS and its app is not in a good state, and I wanted
-        to use it as a proof-of-concept as to how to make an effecive PWA using
-        React as well as Web APIS such as Local Storage
-      </p>
+    <article className=" mx-auto px-4 py-8 max-w-prose">
+      <div className="flex flex-col items-center">
+        <p className="mb-4 text-gray-700">
+          This is a better attempt at an easier-to-use version of the online
+          RADeMS assessment tool.
+        </p>
+        <p className="mb-4 text-gray-700">
+          This tool was created as a tribute to the hard work of the volunteers
+          who have put hours of hard work into the original assessment tool, and
+          isn't intended as a full standalone replacement
+        </p>
+
+        <a
+          className="border-blue-600 border px-4 py-2 rounded-md block text-center"
+          href="#"
+          target="_blank"
+        >
+          <img src={githubIcon} className="w-6 h-6 inline-block mr-2" />
+          Repo
+        </a>
+      </div>
     </article>
   );
 }
