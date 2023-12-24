0f8f1be5f53a1c9f500910bdf920b95601aa3ce4
brianbancroft <hello@brianbancroft.ca>
2023-12-24 13:46:23 -0800

    Add start now button

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import githubIcon from "../lib/github-icon.svg";
 
 function PageAbout() {
@@ -21,6 +22,16 @@ function PageAbout() {
           PWA and distill it down to something that is easier to use in the
           field.
         </p>
+
+        <section className="border-2 border-black rounded my-3 w-full p-4">
+          <h2 className="my-2 text-2xl">New Assessment</h2>
+          <Link
+            to="/assess"
+            className="border-blue-600 border-4 bg-blue-500 text-white px-4 py-2 rounded-md block text-center"
+          >
+            Start
+          </Link>
+        </section>
 
         <a
           className="border-blue-600 border px-4 py-2 rounded-md block text-center"
