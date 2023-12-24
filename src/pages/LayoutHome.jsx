307fb2bf67fd0170e2aec0074728f7c69bffb6ac
brianbancroft <hello@brianbancroft.ca>
2023-12-24 11:06:53 -0800

    Use geist as font

--- src/pages/LayoutHome.jsx
+++ src/pages/LayoutHome.jsx
@@ -5,7 +5,7 @@ import AnimatedPage from "../components/AnimatedPage";
 function LayoutHome() {
   return (
     <>
-      <main className="mb-[125px] md:px-0 px-4">
+      <main className="mb-[125px] md:px-0 px-4 font-geist">
         <AnimatedPage>
           <Outlet />
         </AnimatedPage>
